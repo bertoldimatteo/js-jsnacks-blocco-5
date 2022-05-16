3c0e2ce5315022f4d751bfdbf410831174771083
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-16 12:28:21 +0200

    summ

---
+++ snack-1/main.js
@@ -0,0 +1,64 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina,
+// indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
+const zucchine = [
+    {
+        varietà: "verde",
+        peso: 1,
+        lunghezza: 0.5,
+    },
+    {
+        varietà: "gialla",
+        peso: 1,
+        lunghezza: 1,
+    },
+    {
+        varietà: "blu",
+        peso: 2,
+        lunghezza: 0.55,
+    },
+    {
+        varietà: "arancione",
+        peso: 3,
+        lunghezza: 0.5,
+    },
+    {
+        varietà: "piccola",
+        peso: 2,
+        lunghezza: 0.3,
+    },
+    {
+        varietà: "media",
+        peso: 1,
+        lunghezza: 0.6,
+    },
+    {
+        varietà: "grande",
+        peso: 4,
+        lunghezza: 0.8,
+    },
+    {
+        varietà: "orto",
+        peso: 5,
+        lunghezza: 2.5,
+    },
+    {
+        varietà: "monti",
+        peso: 1,
+        lunghezza: 1,
+    },
+    {
+        varietà: "lago",
+        peso: 2,
+        lunghezza: 0.8,
+    },
+]
+
+let summ = 0;
+
+for (let i = 0; i < zucchine.length; i++){
+    summ =+ zucchine.peso;
+}
+
+console.log(summ);
