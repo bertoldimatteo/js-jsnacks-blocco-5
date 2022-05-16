d82a539ae770ca58749c8440e5022f77815e2797
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-16 12:42:44 +0200

    aggiunto arra zucchine

---
+++ snack-2/main.js
@@ -0,0 +1,56 @@
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+const zucchine = [
+    {
+        varietà: "verde",
+        peso: 1,
+        lunghezza: 15,
+    },
+    {
+        varietà: "gialla",
+        peso: 1,
+        lunghezza: 30,
+    },
+    {
+        varietà: "blu",
+        peso: 2,
+        lunghezza: 20,
+    },
+    {
+        varietà: "arancione",
+        peso: 3,
+        lunghezza: 8,
+    },
+    {
+        varietà: "piccola",
+        peso: 2,
+        lunghezza: 5,
+    },
+    {
+        varietà: "media",
+        peso: 1,
+        lunghezza: 21,
+    },
+    {
+        varietà: "grande",
+        peso: 4,
+        lunghezza: 40,
+    },
+    {
+        varietà: "orto",
+        peso: 5,
+        lunghezza: 2,
+    },
+    {
+        varietà: "monti",
+        peso: 1,
+        lunghezza: 13.5,
+    },
+    {
+        varietà: "lago",
+        peso: 2,
+        lunghezza: 4,
+    },
+]
