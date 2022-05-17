fa6a879c1a4dd1cef809d69862365f3dc2915248
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-17 12:34:09 +0200

    creato array oggetti

---
+++ snack-3/main.js
@@ -0,0 +1,58 @@
+// Crea un array composto da 10 automobili.
+// Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+
+// Infine stampa separatamente i 3 array.
+
+const cars = [
+    {
+        marca: "Audi",
+        modello: "A1",
+        alimentazione: "Benzina"
+    }
+    {
+        marca: "Audi",
+        modello: "A3",
+        alimentazione: "Diesel"
+    }
+    {
+        marca: "Renault",
+        modello: "Clio",
+        alimentazione: "GPL"
+    }
+    {
+        marca: "Fiat",
+        modello: "Panda",
+        alimentazione: "Elettrico"
+    }
+    {
+        marca: "Dacia",
+        modello: "Sandero",
+        alimentazione: "Diesel"
+    }
+    {
+        marca: "Citroen",
+        modello: "C4",
+        alimentazione: "Metano"
+    }
+    {
+        marca: "Nissan",
+        modello: "Qassky",
+        alimentazione: "Benzina"
+    }
+    {
+        marca: "Fiat",
+        modello: "Multipla",
+        alimentazione: "Elettrico"
+    }
+    {
+        marca: "Audi",
+        modello: "Q2",
+        alimentazione: "Metano"
+    }
+    {
+        marca: "Porsche",
+        modello: "Cayenne",
+        alimentazione: "GPL"
+    }
+]
